82a6ce61b61654b20c894011b469495752630adb
Terry MacDonald <terry.macdonald@cosive.com>
2021-09-20 21:50:50 +1200

    Updated version

--- AMDInfo/Program.cs
+++ AMDInfo/Program.cs
@@ -52,10 +52,10 @@ namespace AMDInfo
             NLog.LogManager.Configuration = config;
 
             // Start the Log file
-            SharedLogger.logger.Info($"AMDInfo/Main: Starting AMDInfo v1.0.1");
+            SharedLogger.logger.Info($"AMDInfo/Main: Starting AMDInfo v1.0.2");
 
 
-            Console.WriteLine($"\nAMDInfo v1.0.1");
+            Console.WriteLine($"\nAMDInfo v1.0.2");
             Console.WriteLine($"==============");
             Console.WriteLine($"By Terry MacDonald 2021\n");
 
