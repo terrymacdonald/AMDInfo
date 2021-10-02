304c79775b7f9e8d8cfd36b48d1848dd9321f478
Terry MacDonald <terry.macdonald@cosive.com>
2021-10-03 10:12:46 +1300

    Updated to DisplayMagician v2.0.1.59 AMD library version

--- AMDInfo/AMDLibrary.cs
+++ AMDInfo/AMDLibrary.cs
@@ -1359,7 +1359,7 @@ namespace DisplayMagicianShared.AMD
 
                         }
 
-                    }                    
+                    }
 
                 }
                 else
--- AMDInfo/Program.cs
+++ AMDInfo/Program.cs
@@ -52,10 +52,10 @@ namespace AMDInfo
             NLog.LogManager.Configuration = config;
 
             // Start the Log file
-            SharedLogger.logger.Info($"AMDInfo/Main: Starting AMDInfo v1.0.2");
+            SharedLogger.logger.Info($"AMDInfo/Main: Starting AMDInfo v1.0.3");
 
 
-            Console.WriteLine($"\nAMDInfo v1.0.2");
+            Console.WriteLine($"\nAMDInfo v1.0.3");
             Console.WriteLine($"==============");
             Console.WriteLine($"By Terry MacDonald 2021\n");
 
