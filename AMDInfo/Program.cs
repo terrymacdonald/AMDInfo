55b35297c864d0e81e64e07c5713d7c125dbbac3
Terry MacDonald <terry.macdonald@cosive.com>
2022-06-12 22:00:57 +1200

    Added validation to TaskBarLayout
    Also updated to version 1.8.0

--- AMDInfo/Program.cs
+++ AMDInfo/Program.cs
@@ -52,10 +52,10 @@ namespace AMDInfo
             NLog.LogManager.Configuration = config;
 
             // Start the Log file
-            SharedLogger.logger.Info($"AMDInfo/Main: Starting AMDInfo v1.7.9");
+            SharedLogger.logger.Info($"AMDInfo/Main: Starting AMDInfo v1.8.0");
 
 
-            Console.WriteLine($"\nAMDInfo v1.7.9");
+            Console.WriteLine($"\nAMDInfo v1.8.0");
             Console.WriteLine($"==============");
             Console.WriteLine($"By Terry MacDonald 2022\n");
 
